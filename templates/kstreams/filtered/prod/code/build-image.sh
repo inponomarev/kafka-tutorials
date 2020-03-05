ff5c3b8fd5b32f43f7bd66e319f6719e5bd2ed02
Viktor Gamov <viktor@confluent.io>
2020-03-05 10:28:47 -0500

    Removed leftovers from generating script

--- templates/kstreams/filtered/prod/code/build-image.sh
+++
@@ -1,1 +0,0 @@
-gradle jibDockerBuild --image=io.confluent.developer/<TUTORIAL-SHORT-NAME>:0.0.1
--- templates/kstreams/static/prod/code/build-image.sh
+++
@@ -1,1 +0,0 @@
-gradle jibDockerBuild --image=io.confluent.developer/<TUTORIAL-SHORT-NAME>-join:0.0.1
